68d3d61c7e8686f6edc6e58b4df57969dadb8ecf
Martín Arroyo <maredarro@gmail.com>
2025-11-09 11:25:20 -0300

    Agrege

--- bodega/settings.py
+++ bodega/settings.py
@@ -77,11 +77,11 @@ WSGI_APPLICATION = 'bodega.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'bodega_db',      
-        'USER': 'postgres',       
-        'PASSWORD': '1234',       
-        'HOST': 'localhost',      
-        'PORT': '5432',           
+        'NAME': os.getenv("DB_NAME"),   
+        'USER': os.getenv("DB_USER"),  
+        'PASSWORD': os.getenv("DB_PASSWORD"),     
+        'HOST':  os.getenv("DB_HOST"),    
+        'PORT': os.getenv("DB_PORT", "5432"),
     }
 }
 
